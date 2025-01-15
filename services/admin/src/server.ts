297ba2a35303432ad70a007756a821b6e0590deb
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-01-15 10:39:14 +0530

    feat: env checker util fn

--- services/admin/src/server.ts
+++ services/admin/src/server.ts
@@ -1,12 +1,16 @@
 // dotenv config
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app";
+import { isEnvDefined } from "./utils/envChecker";
 
 // Start Server
 const startServer = async () => {
     try {
+        // Check all env are defined
+        isEnvDefined()
+
         // port listening
         app.listen(process.env.PORT, () => {
             console.log(`Admin service running on port ${process.env.PORT}`);
---
+++ services/admin/src/utils/envChecker.ts
@@ -0,0 +1,11 @@
+import { envChecker } from "@codeflare/common";
+/**
+ * Checks if all the required environment variables are defined.
+ * @returns {void} nothing
+ */
+export const isEnvDefined = () => {
+    envChecker(process.env.PORT as string, "PORT");
+    // envChecker(process.env.MONGO_DB_URL as string, "MONDO_DB_URL");
+    // envChecker(process.env.JWT_REFRESH_TOKEN_SECRET as string, "JWT_REFRESH_TOKEN_SECRET");
+    // envChecker(process.env.JWT_ACCESS_TOKEN_SECRET as string, "JWT_ACCESS_TOKEN_SECRET");
+};
