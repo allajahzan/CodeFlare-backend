9f6f68526dba7252a9744d96af3682a4f720fb1e
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-01-15 11:14:35 +0530

    patch: admin service conncted to db

--- services/admin/src/server.ts
+++ services/admin/src/server.ts
@@ -4,12 +4,17 @@ dotenv.config();
 
 import app from "./app";
 import { isEnvDefined } from "./utils/envChecker";
+import { MongodbConnection } from "@codeflare/common";
 
 // Start Server
 const startServer = async () => {
     try {
         // Check all env are defined
-        isEnvDefined()
+        isEnvDefined();
+
+        // Connect to MongoDB
+        const db = new MongodbConnection(process.env.MONGO_DB_URL as string);
+        await db.retryConnection();
 
         // port listening
         app.listen(process.env.PORT, () => {
