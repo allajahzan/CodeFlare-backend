20f9191de52a0c2b8d6a60ab0f933c20d0f1093b
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-01-15 13:10:47 +0530

    feat: user repository and interface

---
+++ services/admin/src/repository/implementation/userRepository.ts
@@ -0,0 +1,60 @@
+import { BaseRepository } from "@codeflare/common";
+import { IUserSchema } from "../../modal/interface/IUserSchema";
+import { IUserRepository } from "../interface/IUserRepository";
+import { Model } from "mongoose";
+
+/** Implementation of User Repository */
+export class UserRepository extends BaseRepository<IUserSchema> implements IUserRepository {
+    /**
+     * Constructs an instance of UserRepository.
+     * @param model - The mongoose model representing the user schema, used for database operations.
+     */
+    constructor(model: Model<IUserSchema>) {
+        super(model);
+    }
+
+    /**
+     * Finds a single user in the database that matches the given email.
+     * @param email - The email to filter the documents.
+     * @returns A promise that resolves to the user that matches the email if found, otherwise null.
+     */
+    async findUserByEmail(email: string): Promise<IUserSchema | null> {
+        try {
+            return await this.model.findOne({ email });
+        } catch (err: any) {
+            return null;
+        }
+    }
+
+    /**
+     * Blocks a user with the given _id.
+     * @param _id - The id of the user to block.
+     * @returns A promise that resolves to the updated user if the blocking is successful, otherwise null.
+     */
+    async blockUser(_id: string): Promise<IUserSchema | null> {
+        try {
+            return await this.model.findOneAndUpdate(
+                { _id },
+                { $set: { isblock: true } }
+            );
+        } catch (err: any) {
+            return null;
+        }
+    }
+
+    /**
+     * Unblocks a user with the given _id.
+     * @param _id - The id of the user to unblock.
+     * @returns A promise that resolves to the updated user if the unblocking is successful, otherwise null.
+     */
+    async unblockUser(_id: string): Promise<IUserSchema | null> {
+        try {
+            return await this.model.findOneAndUpdate(
+                { _id },
+                { $set: { isblock: false } }
+            );
+        } catch (err: any) {
+            return null;
+        }
+    }
+}
