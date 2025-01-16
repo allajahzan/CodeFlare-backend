25e0c8514fedaa3e10a141997deb3cac2a8bbdec
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-01-16 22:03:33 +0530

    feat: adminRoute and dependency injuction

---
+++ services/admin/src/routes/adminRoute.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { AdminController } from "../controller/implementation/adminController";
+import { AdminService } from "../service/implementation/adminService";
+import { AdminRepositoty } from "../repository/implementation/adminRepository";
+import Admin from "../modal/admin";
+
+const router = Router();
+
+// Dependency Injection
+const adminRepository = new AdminRepositoty(Admin);
+const adminService = new AdminService(adminRepository);
+const adminController = new AdminController(adminService);
+
+// Get admin
+router.get("/:id", (req: Request, res: Response, next: NextFunction) =>
+    adminController.getAdmin(req, res, next)
+);
+
+// Update admin
+router.put("/:id", (req: Request, res: Response, next: NextFunction) =>
+    adminController.updateAdmin(req, res, next)
+);
+
+export { router as adminRoute };
