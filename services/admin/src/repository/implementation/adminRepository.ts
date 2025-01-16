01d4ff6e6b28a5f39381d3553da27ae6d294091e
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-01-16 17:39:49 +0530

    feat: adminRepository and interface

---
+++ services/admin/src/repository/implementation/adminRepository.ts
@@ -0,0 +1,15 @@
+import { BaseRepository } from "@codeflare/common";
+import { IAdminSchema } from "../../modal/interface/IAdminSchema";
+import { Model } from "mongoose";
+import { IAdminRepostory } from "../interface/IAdminRepository";
+
+/** Implementation of Admin Repository */
+export class AdminRepositoty extends BaseRepository<IAdminSchema> implements IAdminRepostory {
+    /**
+     * Constructs an instance of AdminRepository.
+     * @param model - The mongoose model representing the admin schema, used for database operations.
+     */
+    constructor(model: Model<IAdminSchema>) {
+        super(model);
+    }
+}
---
+++ services/admin/src/repository/interface/IAdminRepository.ts
@@ -0,0 +1,5 @@
+import { IBaseRepository } from "@codeflare/common";
+import { IAdminSchema } from "../../modal/interface/IAdminSchema";
+
+/** Inteface for Admin Repository */
+export interface IAdminRepostory extends IBaseRepository<IAdminSchema> {}
