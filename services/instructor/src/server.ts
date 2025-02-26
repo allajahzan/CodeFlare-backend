7597ca1dd6b63fc4be916fd3b35d434bf4e1a167
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-02-26 15:13:35 +0530

    feat: gave mongo db connection

---
+++ services/instructor/src/server.ts
@@ -0,0 +1,35 @@
+// dotenv config
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app";
+import { connectRedis, MongodbConnection } from "@codeflare/common";
+import { isEnvDefined } from "./utils/envChecker";
+// import { startGrpcServer } from "./grpc/grpc.server";
+
+// server
+const startServer = async () => {
+    try {
+        // check all env are defined
+        isEnvDefined();
+
+        // connect to mongodb
+        const db = new MongodbConnection(process.env.MONGO_DB_URL as string);
+        await db.retryConnection();
+
+        // connect to redis
+        connectRedis();
+
+        // start grpc server
+        // startGrpcServer()
+
+        //listen to port
+        app.listen(process.env.PORT, () =>
+            console.log("Instructor service running on port 3002")
+        );
+    } catch (err: any) {
+        console.log(err.message);
+    }
+};
+
+startServer();
