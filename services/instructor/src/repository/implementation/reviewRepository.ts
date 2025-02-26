80d609c985e6a0598074d45a30782849bd42bd66
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-02-26 16:38:36 +0530

    patch: corrected the review repository

--- services/instructor/src/repository/implementation/reviewRepository.ts
+++ services/instructor/src/repository/implementation/reviewRepository.ts
@@ -1,6 +1,15 @@
 import { BaseRepository } from "@codeflare/common";
 import { IReviewSchema } from "../../entities/IReviewSchema";
 import { IReviewRepository } from "../interface/IReviewRepository";
+import { Model } from "mongoose";
 
 /** Implementation of Review Repository */
-export class ReviewRepository extends BaseRepository<IReviewSchema> implements IReviewRepository{}
+export class ReviewRepository extends BaseRepository<IReviewSchema> implements IReviewRepository{
+    /**
+     * Constructs an instance of the ReviewRepository.
+     * @param model - The mongoose model representing the review schema, used for database operations.
+     */
+    constructor(model: Model<IReviewSchema>){
+        super(model)
+    }
+}
