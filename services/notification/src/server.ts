2d7fb501eff5a416f09a19022c68ae8b77aa0119
Ahsan allaj pk <ahsanallajpk22@gmail.com>
2025-04-13 16:42:37 +0530

    feat: implemented rabbitmq connection

--- services/notification/src/server.ts
+++ services/notification/src/server.ts
@@ -3,10 +3,23 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app";
+import { MongodbConnection } from "@codeflare/common";
+import { isEnvDefined } from "./utils/envChecker";
+import { rabbitmq } from "./config/rabbitmq";
 
 // Start server
-const startServer = () => {
+const startServer = async () => {
     try {
+        // check all env are defined
+        isEnvDefined();
+
+        // connect to mongodb
+        const db = new MongodbConnection(process.env.MONGO_DB_URL as string);
+        await db.retryConnection();
+
+        // connect to rabbitmq
+        await rabbitmq.connect();
+
         //listen to port
         app.listen(process.env.PORT, () => {
             console.log(`Notification service running on port ${process.env.PORT}`);
